6ca093f00e9edc4c4320d09db524ac14a787f893
wouter <0934955@hr.nl>
2017-11-13 11:33:41 +0100

    Models added and Db created

---
+++ Migrations/20171113102603_InitialCreateWeeGamesDb.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WeeGames.Migrations
+{
+    public partial class InitialCreateWeeGamesDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int4", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Genre = table.Column<string>(type: "text", nullable: true),
+                    Platform = table.Column<string>(type: "text", nullable: true),
+                    Price = table.Column<int>(type: "int4", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WeeGames.Models
+{
+public class GameContext : DbContext {
+        public DbSet<Game> Games {get; set;}
+
+        public GameContext(DbContextOptions<GameContext> options)
+        :base(options)
+        {}
+    }
+
+    public class Game{
+        public int Id {get;set;}
+        public string Title {get;set;}
+        public string Genre {get;set;}
+        public int Price {get;set;}
+        public string Platform {get;set;}
+    }
+}
+
