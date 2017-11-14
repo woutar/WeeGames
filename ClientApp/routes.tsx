dfeabcab284d514b1605725d7388284271a8b025
BrkanDrmaz <0920641@hr.nl>
2017-11-14 15:21:05 +0100

    Routes

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -5,6 +5,7 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { FetchAllGames } from './components/FetchAllGames';
+import { Product } from './components/Product';
 import { Imaget } from './components/Imaget';
 import { Shoppingcart } from './components/Shoppingcart';
 
@@ -13,6 +14,7 @@ export const routes = <Layout>
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata' component={ FetchData } /> */}
     <Route exact path='/' component={ FetchAllGames } />
-    <Route exact path='/Shoppingcart' component={ Shoppingcart } />
+    <Route path='/productpage/:id' component={ Product } />
+    <Route path='/Shoppingcart/' component={ Shoppingcart } />
 </Layout>;
 
