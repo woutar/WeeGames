5dded87a6bd646a34982083143a08046a0be5c65
Kees <keesgoed@hotmail.com>
2017-11-20 12:33:07 +0100

    trying to read url input

--- ClientApp/components/FetchSearchResult.tsx
+++ ClientApp/components/FetchSearchResult.tsx
@@ -3,10 +3,24 @@ import { RouteComponentProps } from 'react-router';
 import { Route, NavLink, Link } from 'react-router-dom';
 import * as Models from "../Model"
 
-export class FetchSearchResult extends React.Component<RouteComponentProps<{searchquery : string}>>{
-    constructor(props:RouteComponentProps<(searchquery: string)>){
+interface test{
+    test: string;
+}
+
+export class FetchSearchResult extends React.Component<RouteComponentProps<{searchquery : string}>, test>{
+    constructor(props:RouteComponentProps<{searchquery:string}>){
         super(props);
+        this.state = {test: ''}
+
+        this.setState({test: this.props.match.params.searchquery });
+        // let querystring = this.props.match.params.searchquery;
+        alert(this.state.test)
 
     }
+
+    public render() {
+        return <div></div>
+    }
+
 
 }
