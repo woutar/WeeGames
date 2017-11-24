1cc9f47128c4a9ff38497e755a435dfeb0c335e7
Kees <keesgoed@hotmail.com>
2017-11-24 15:17:08 +0100

    Add test products

--- Models/Model.cs
+++ Models/Model.cs
@@ -20,7 +20,7 @@ public class GameContext : DbContext {
     public class Game{
         public int Id {get;set;}
         public string Title {get;set;}
-        public int Price {get;set;}
+        public double Price {get;set;}
         public string Description {get;set;}
         public int CategoryId {get;set;}   
         public Category Category {get;set;}
