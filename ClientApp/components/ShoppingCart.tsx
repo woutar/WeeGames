32c62a6c494be7a162760f5c42cb61884a8fd25d
Yori Giacalone <0903577@hr.nl>
2017-12-06 13:32:57 +0100

    Shoppingcart layout fix

--- ClientApp/components/ShoppingCart.tsx
+++ ClientApp/components/ShoppingCart.tsx
@@ -39,28 +39,47 @@ export class ShoppingCart extends React.Component<RouteComponentProps<{}>, {game
     }
 
     public render() {
-        return <div className="col-lg-8">
+        return <div className="col-md-10 content">
         <h2>Shoppingcart</h2>
         {this.state.games.map(game =>
-        <div className="product" key= { game.id }>
-            <Link to={"/game/" + game.id}>
-            <div className="col-lg-3">
-                <img height="150" width="150" src={game.image} /> 
-            </div>
-                <div className="col-lg-9">
-                    <div className="product-info">
-                        <ul>
-                            <li className="title">{ game.title }</li>
-                            <li className="genre">Genre: { game.category.name }</li>
-                            <li className="platform">Platform: {game.platform.name }</li>
-                            <li className="prijs">Price: &euro;{ game.price }</li>
-                            <li className="prijs">Amount: { game.amount }</li>
-                            <li className="prijs">Subtotal: &euro; { game.amount * game.price }</li>
-                        </ul>
+            <div className="row product" key={ game.id }>
+                <Link to={"/game/" + game.id}>
+                    <div className="row">
+                        <div className="col-sm-3">
+                            <img src={game.image} />
+                        </div>
+                        <div className="col-sm-9">
+                            <div className="row">
+                                <div className="col-sm-12">
+                                    <span className="title">{ game.title }</span>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-12">
+                                    <span className="tag">Categorie: </span><span className="category">{ game.category }</span>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-12">
+                                    <span className="tag">Price: </span><span className="price">{ game.price }</span>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-12">
+                                    <span className="tag">Platform: </span><span className="platform">{ game.platform }</span>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-sm-12">
+                                    <span className="description">
+                                        { game.description.substring(0, 250) }...
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </Link>
                 </div>
-            </Link>
-        </div>
         )}
 
         <div className="checkout-wrapper">
