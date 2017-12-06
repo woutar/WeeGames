67e502ff87683d24c2c051cbe4a25c24027625b5
wouter <0934955@hr.nl>
2017-12-06 14:30:02 +0100

    Login and register have callbacks now

--- ClientApp/components/Login.tsx
+++ ClientApp/components/Login.tsx
@@ -4,82 +4,90 @@ import { Route, NavLink, Link } from 'react-router-dom';
 import * as CryptoJS from 'crypto-js';
 import * as Models from "../Model";
 
-    interface userState{
-        userEmail: string;
-        userPassword: string;
-        user : Models.User | '';
+interface userState{
+    userEmail: string;
+    userPassword: string;
+    user : Models.User | '';
+}       
+
+export class Login extends React.Component<RouteComponentProps<{}>, userState> {
+    constructor(){
+        super();
+
+        this.state = {userEmail: '', userPassword:  '', user : ''};
+
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
     }
 
-    export class Login extends React.Component<RouteComponentProps<{}>, userState> {
-        constructor(){
-            super();
+    handleEmailChange(event: any){
+        this.setState({
+            userEmail: event.target.value
+        })
+    }
 
-            this.state = {userEmail: '', userPassword:  '', user : ''};
+    handlePasswordChange(event: any){
+        this.setState({
+            userPassword: event.target.value
+        })
 
-            this.handleEmailChange = this.handleEmailChange.bind(this);
-            this.handlePasswordChange = this.handlePasswordChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-
-        }
-
-        handleEmailChange(event: any){
-            this.setState({
-                userEmail: event.target.value
+    }
+    handleSubmit(event : any){
+        event.preventDefault();
+        fetch('api/User/Login',{
+            method : 'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Email : this.state.userEmail,
+                Password : CryptoJS.SHA256(this.state.userPassword).toString(),
             })
-        }
-
-        handlePasswordChange(event: any){
-            this.setState({
-                userPassword: event.target.value
-            })
-
-        }
-        handleSubmit(event: any){
-            event.preventDefault();
-            fetch('api/User/Login',{
-                method : 'POST',
-                headers:{
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    Email : this.state.userEmail,
-                    Password : CryptoJS.SHA256(this.state.userPassword).toString(),
-                })
-            }).then(response => response.json() as Promise<Models.User>)
-            .then(data => {
-                this.setState({ user: data });
-            });
+        }).then(this.errorHandler).then(response => response.json() as Promise<Models.User>)
+        .then(data => {
+            this.setState({ user: data });
             if(this.state.user != '')
             {
-                alert("You are logged in");
-            }else{
-                alert("Something went wrong ");
+                alert("You are logged in " + this.state.user.firstname);
+                sessionStorage.user = JSON.stringify(this.state.user)
+                window.location.href = "/user";
             }
+        });
+    }
+    errorHandler(response:any){
+        if(response.status == 200){
+            return response;
+        }else{
+            alert("User could not be found");
         }
 
-        render() {
-            return (
-            <div className="row">
-            <form onSubmit={this.handleSubmit}>
-                <div className="col-lg-4 col-lg-offset-4">
-                    <h2>Login</h2>
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input name="userEmail" type="email" className="form-control" value={this.state.userEmail}
-                         onChange={this.handleEmailChange}  placeholder="Enter email" />
-                    </div>
+    }
 
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input name="userPassword" type="password" className="form-control" value={this.state.userPassword} 
-                        onChange={this.handlePasswordChange}  placeholder="******" />
-                    </div>
-               
-                    <input type="submit" className="btn btn-default"  value="Submit"/>
+    render() {
+        return (
+        <div className="row">
+        <form onSubmit={this.handleSubmit}>
+            <div className="col-lg-4 col-lg-offset-4">
+                <h2>Login</h2>
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input name="userEmail" type="email" className="form-control" value={this.state.userEmail}
+                        onChange={this.handleEmailChange}  placeholder="Enter email" />
                 </div>
-            </form>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input name="userPassword" type="password" className="form-control" value={this.state.userPassword} 
+                    onChange={this.handlePasswordChange}  placeholder="******" />
+                </div>
+            
+                <input type="submit" className="btn btn-default"  value="Submit"/>
             </div>
+        </form>
+        </div>
         )
     }
 }
--- ClientApp/components/Register.tsx
+++ ClientApp/components/Register.tsx
@@ -13,124 +13,140 @@ interface PostUserState {
     Address : string,
     Zipcode : string,
     Country : string,
-    Role : number
+    Role : number,
+    createdUser : Models.User | '',
+    userCreated : boolean
 }
 
-    export class Register extends React.Component<RouteComponentProps<{}>,PostUserState>{
-        constructor(){
-            super();
+export class Register extends React.Component<RouteComponentProps<{}>,PostUserState>{
+    constructor(props :any){
+        super(props);
 
-            this.state ={
-                Email : '',
-                Password : '',
-                Firstname : '',
-                Lastname : '',
-                Birthdate : new Date(),
-                Address : '',
-                Zipcode : '',
-                Country : 'Netherlands',
-                Role : 0
-            };
+        this.state ={
+            Email : '',
+            Password : '',
+            Firstname : '',
+            Lastname : '',
+            Birthdate : new Date(),
+            Address : '',
+            Zipcode : '',
+            Country : 'Netherlands',
+            Role : 0,
+            createdUser : '',
+            userCreated : false
+        };
 
-            this.handleInputChange = this.handleInputChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-        }
-    
-        handleInputChange(event : any){
-            const target = event.target;
-            const value = target.value;
-            const name = target.name;
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-            this.setState({
-                [name] : value
-            });
-        }
+    handleInputChange(event : any){
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
 
-        handleSubmit(event : any){
-            fetch('api/User/Post',{
-                method : 'POST',
-                headers:{
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    Email : this.state.Email,
-                    Password : CryptoJS.SHA256(this.state.Password).toString(),
-                    Firstname : this.state.Firstname,
-                    Lastname : this.state.Lastname,
-                    Birthdate : this.state.Birthdate,
-                    Address : this.state.Address,
-                    Zipcode : this.state.Zipcode,
-                    Country : this.state.Country,
-                    Role : this.state.Role
-                })
+        this.setState({
+            [name] : value
+        });
+    }
+
+    handleSubmit(event : any){
+        event.preventDefault();
+        fetch('api/User/Register',{
+            method : 'POST',
+            headers:{
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                Email : this.state.Email,
+                Password : CryptoJS.SHA256(this.state.Password).toString(),
+                Firstname : this.state.Firstname,
+                Lastname : this.state.Lastname,
+                Birthdate : this.state.Birthdate,
+                Address : this.state.Address,
+                Zipcode : this.state.Zipcode,
+                Country : this.state.Country,
+                Role : this.state.Role
             })
-            event.preventDefault();
-        }
-
+        })
+        .then(response => response.json() as Promise<Models.User>)
+        .then(data => {
+            this.setState({ createdUser: data, userCreated : true});
+            this.userCreated();
+        });
         
-        render() {
-            return (
-            <div className="row">
-            <h2>Register</h2>
-            <form method="post" onSubmit={this.handleSubmit}>
-                <div className="col-lg-4">
-                    <div className="form-group">
-                        <label>Email address</label>
-                        <input name="Email" id="Email" type="email" className="form-control" placeholder="Enter email" required
-                        onChange ={this.handleInputChange}
-                        />
-                    </div>
+    }
+    
+    userCreated(){
+        if(this.state.userCreated && this.state.createdUser != ''){
+            sessionStorage.user = JSON.stringify(this.state.createdUser)
+            alert("User created");
+            window.location.href = "/user";
+        }
+    }
 
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input name="Password" id="Password" type="password" className="form-control" placeholder="******" 
-                        onChange ={this.handleInputChange}
-                        />
-                    </div>
+    render() {
+        return <div className="row">
+        <h2>Register</h2>
+        <form method="post" onSubmit={this.handleSubmit}>
+            <div className="col-lg-4">
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input name="Email" id="Email" type="email" className="form-control" placeholder="Enter email" required
+                    onChange ={this.handleInputChange}
+                    />
                 </div>
-                <div className="col-lg-2" />
-                <div className="col-lg-4">
-                    <div className="form-group">
-                        <label>Firstname</label>
-                        <input name="Firstname" id="Firstname" type="text" className="form-control"  placeholder="Enter Firstname"
-                        onChange ={this.handleInputChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Lastname</label>
-                        <input name="Lastname" id="Lastname" type="text" className="form-control"  placeholder="Enter Lastname"
-                        onChange ={this.handleInputChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Birthdate</label>
-                        <input name="Birthdate" id="Birthdate" type="date" className="form-control"  placeholder="Enter Birthdate"
-                        onChange ={this.handleInputChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Address</label>
-                        <input name="Address" id="Address" type="text" className="form-control"  placeholder="Enter Address"
-                        onChange ={this.handleInputChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Zipcode</label>
-                        <input name="Zipcode" id="Zipcode" type="text" className="form-control"  placeholder="Enter Zipcode"
-                        onChange ={this.handleInputChange}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Country</label>
-                        <br/>
-                        <select name="Country" id="Country" className="form-control" value={this.state.Country} onChange ={this.handleInputChange}>
-                            <option value="Netherlands">Netherlands</option>
-                            <option value="France">France</option>
-                            <option value="UK">United Kingdom</option>
-                            <option value="Germany">Germany</option>
-                            <option value="Belgium">Belgium</option>
-                        </select>
-                    </div>
-                    <input type="submit" className="btn btn-default" value="Submit"/>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input name="Password" id="Password" type="password" className="form-control" placeholder="******" 
+                    onChange ={this.handleInputChange}
+                    />
                 </div>
-            </form>
             </div>
-        )
+            <div className="col-lg-2" />
+            <div className="col-lg-4">
+                <div className="form-group">
+                    <label>Firstname</label>
+                    <input name="Firstname" id="Firstname" type="text" className="form-control"  placeholder="Enter Firstname"
+                    onChange ={this.handleInputChange}/>
+                </div>
+                <div className="form-group">
+                    <label>Lastname</label>
+                    <input name="Lastname" id="Lastname" type="text" className="form-control"  placeholder="Enter Lastname"
+                    onChange ={this.handleInputChange}/>
+                </div>
+                <div className="form-group">
+                    <label>Birthdate</label>
+                    <input name="Birthdate" id="Birthdate" type="date" className="form-control"  placeholder="Enter Birthdate"
+                    onChange ={this.handleInputChange}/>
+                </div>
+                <div className="form-group">
+                    <label>Address</label>
+                    <input name="Address" id="Address" type="text" className="form-control"  placeholder="Enter Address"
+                    onChange ={this.handleInputChange}/>
+                </div>
+                <div className="form-group">
+                    <label>Zipcode</label>
+                    <input name="Zipcode" id="Zipcode" type="text" className="form-control"  placeholder="Enter Zipcode"
+                    onChange ={this.handleInputChange}/>
+                </div>
+                <div className="form-group">
+                    <label>Country</label>
+                    <br/>
+                    <select name="Country" id="Country" className="form-control" value={this.state.Country} onChange ={this.handleInputChange}>
+                        <option value="Netherlands">Netherlands</option>
+                        <option value="France">France</option>
+                        <option value="UK">United Kingdom</option>
+                        <option value="Germany">Germany</option>
+                        <option value="Belgium">Belgium</option>
+                    </select>
+                </div>
+                <input type="submit" className="btn btn-default" value="Submit"/>
+            </div>
+        </form>
+        </div>
+        
     }
 }
